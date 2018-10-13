7448025bb20e27a34f84703c2fb1b84321fec429
Cambron Deatherage <ctd1077@gmail.com>
2018-10-13 11:48:01 -0600

    Update JS with code

--- designs.js
+++ designs.js
@@ -1,10 +1,46 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
+    var cellColor;
+    var nRow;
+    var selectedCell;
+    var height = $('#inputHeight').val();
+    var width = $('#inputWidth').val();
+    var grid = $('#pixelCanvas');
 
-// Your code goes here!
 
+    grid.children().remove();
+
+//This for loop creates the tables rows
+    for (var trowCount = 0; trowCount < height; trowCount++) {
+        grid.append('<tr></tr>');
+    }
+
+    nRow = $('tr');
+
+    //This for loop creates the table cells
+    for (var cellCount = 0; cellCount < width; cellCount++) {
+        nRow.append('<td></td>');
+    }
+
+  selectedCell = grid.find('td');
+
+	// This fuction selects the color when the
+  // cell is clicked on by the user
+	selectedCell.click(function() {
+		cellColor = $('#colorPicker').val();
+        if ($(this).attr('bgcolor')) {
+            $(this).removeAttr('bgcolor')
+        } else {
+            $(this).attr('bgcolor', cellColor);
+        }
+	});
 }
+
+// This calles the makegrid function
+// when the user submits the grid size
+$(document).ready(function() {
+    var createGrid = $('input[type="submit"]');
+    createGrid.click(function(event) {
+        event.preventDefault();
+        makeGrid();
+    });
+});
